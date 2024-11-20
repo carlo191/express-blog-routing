46a05139a9bbb8530f6af1dc0cca3caed9cee059
Carlo Elvisi <carloelvisi01@outlook.it>
2024-11-20 15:17:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Server del mio blog");
+});
+
+app.listen(port, () => {
+  console.log(`In ascolto su port  ${port}`);
+});
+
+// array di ogetti
+const posts = [
+  {
+    titolo: "Ciambellone",
+    contenuto: "ciambellone",
+    immagine: "ciambellone.jpeg",
+    tags: ["Ciambellone", "ciambella", "grande"],
+  },
+  {
+    titolo: "Cracker Barbabietola",
+    contenuto: "Cracker Barbabietola",
+    immagine: "cracker_barbabietola.jpeg",
+    tags: ["cracker", "barbabietola"],
+  },
+  {
+    titolo: "Pane Fritto Dolce",
+    contenuto: "Pane Fritto Dolce",
+    immagine: "pane_fritto_dolce.jpeg",
+    tags: ["pane", "fritto", "dolce"],
+  },
+  {
+    titolo: "Pasta Barbabietola",
+    contenuto: "Pasta Barbabietola",
+    immagine: "pasta_barbabietola.jpeg",
+    tags: ["pasta", "barbabietola"],
+  },
+  {
+    titolo: "Torta Paesana",
+    contenuto: "Torta Paesana",
+    immagine: "torta_paesana.jpeg",
+    tags: ["torta", "paesana"],
+  },
+];
+// rotta /bacheca
+app.get("/bacheca", (req, res) => {
+  const response = {
+    conteggio: posts.length,
+    post: posts,
+  };
+
+  res.json(response);
+});
